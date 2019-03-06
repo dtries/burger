6b0ef8298de5d2c28ac9d5e96c016b46851aaceb
Dennis Ries <dtries@gmail.com>
2019-03-06 10:04:36 -0700

    added heroku jaswdb back in code

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require("mysql");
 
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.DB_PORT,
@@ -12,7 +12,7 @@ var connection;
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
   });
-// };
+};
 
 connection.connect(function (err) {
   if (err) {
