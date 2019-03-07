40ccafb5de4691811573db05b5d5db15deb39ef5
Dennis Ries <dtries@gmail.com>
2019-03-07 01:26:44 -0700

    Set autocomplete to off for text entry, removed unused images

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,6 @@ router.get("/", function (req, res) {
         var hbsObject = {
             burgers: data
         };
-        // console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
@@ -26,8 +25,6 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
-    // console.log("condition", condition);
     burger.updateOne({
         devoured: true
     }, condition, function (data) {
